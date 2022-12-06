9396167c41073cee4373aa327d7205c4ef3bb9da
mrslwiseman <mrslwiseman@gmail.com>
2022-12-06 22:26:26 +1030

    Day 1, part 1 + 2

---
+++ 2022/1/index.test.ts
@@ -0,0 +1,25 @@
+import test from "node:test";
+import assert from "assert/strict";
+import { main1, main2 } from "./index";
+
+test("part 1", async (t) => {
+  await t.test("must return highest count", async () => {
+    const result = await main1("example");
+    assert.equal(result, 24000);
+  });
+  await t.test("must return highest count", async () => {
+    const result = await main1("real");
+    assert.equal(result, 69177);
+  });
+});
+
+test("part 2", async (t) => {
+  await t.test("must return sum of highest 3 counts", async () => {
+    const result = await main2("example");
+    assert.equal(result, 45000);
+  });
+  await t.test("must return sum of highest 3 counts", async () => {
+    const result = await main2("real");
+    assert.equal(result, 207456);
+  });
+});
