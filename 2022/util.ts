d286e762f836cf2ec319651bf95b781db48111d8
Matt Wiseman <matt.wiseman@sine.co>
2022-12-12 22:30:18 +1100

    Day 2, part 2, ditched node:test in favour of jest

--- 2022/util.ts
+++ 2022/util.ts
@@ -1,9 +1,12 @@
 import { readFile } from "fs/promises";
+import path from "node:path";
 
 export type InputFileType = "example" | "real";
 
-export async function getInput(file: InputFileType) {
+export async function getInput(part: number, file: InputFileType) {
   const filePath = file === "example" ? "input.example.txt" : "input.txt";
-  const data = await readFile(`./${filePath}`, { encoding: "utf-8" });
+  const data = await readFile(path.join(__dirname, `${part}`, `${filePath}`), {
+    encoding: "utf-8",
+  });
   return data.toString();
 }
