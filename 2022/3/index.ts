cdbd8aae8e93e3473c0e6a2ad6838ff4eafed119
Matt Wiseman <matt.wiseman@sine.co>
2022-12-13 16:24:58 +1100

    Day 3, part 1,2

--- 2022/3/index.ts
+++ 2022/3/index.ts
@@ -1,154 +1,94 @@
 import { getInput } from "../util";
 import type { InputFileType } from "../util";
 
-type HandType = "rock" | "paper" | "scissors";
-
-// Represents which hand beats which hand eg rock: beats scissors
-export const ranks: Record<string, HandType[]> = {
-  rock: ["scissors"],
-  paper: ["rock"],
-  scissors: ["paper"],
-};
-
-const scores: Record<string, number> = {
-  rock: 1,
-  paper: 2,
-  scissors: 3,
-};
-
-function getHand(hand: string): HandType {
-  switch (hand) {
-    case "A":
-    case "X":
-      return "rock";
-    case "B":
-    case "Y":
-      return "paper";
-    case "C":
-    case "Z":
-      return "scissors";
-    default:
-      throw Error(`unknown hand type ${hand}`);
-  }
+export function splitCompartments(items: string) {
+  return [
+    items.slice(0, items.length / 2).split(""),
+    items.substring(items.length / 2).split(""),
+  ];
 }
 
-// Todo: refactor by limiting the amount of iteration here ( im sleepy )
+export function findCommonItems(a: string[], b: string[]) {
+  const bSet = new Set(b);
+  for (const item of a) {
+    if (bSet.has(item)) {
+      return item;
+    }
+  }
+  throw new Error("no common item found");
+}
+
+export function getPriority(character: string) {
+  return character.charCodeAt(0) - (/[A-Z]/.test(character) ? 38 : 96);
+}
+
 export async function main1(file: InputFileType) {
-  const data = await getInput(2, file);
+  const data = await getInput(3, file);
 
-  const turns = data
-    .split("\n")
-    .filter((line) => /[ABCXYZ]/.test(line))
-    .map((line) => {
-      const turns = line.split(" ");
-
-      const [p1Score, p2Score] = turns.map((turn) => scores[getHand(turn)]);
-      const [p1Wins, p2Wins] = turns.map((turn, i) =>
-        ranks[getHand(turn)].includes(getHand(turns[i + 1] || turns[0]))
-      );
-
-      const isDraw = p1Score === p2Score;
-
-      const result = {
-        p1Score: p1Score + (p1Wins ? 6 : isDraw ? 3 : 0),
-        p2Score: p2Score + (p2Wins ? 6 : isDraw ? 3 : 0),
-      };
-
-      return result;
-    })
-    .reduce(
-      (acc, curr) => {
-        acc.p1Score += curr.p1Score;
-        acc.p2Score += curr.p2Score;
-
-        return acc;
-      },
-      { p1Score: 0, p2Score: 0 }
-    );
-  return turns.p2Score;
-}
-
-export function getSuperiorHand(losingTurn: string) {
-  const result = Object.keys(ranks).find((rank) =>
-    ranks[rank].includes(getHand(losingTurn))
-  );
-
-  if (!result) {
-    throw Error(`couldnt find superior hand to ${losingTurn}`);
-  }
-
-  // console.log(`superior: ${result} beats: ${losingTurn}`);
-
-  return {
-    outcome: "win",
-    result,
-    score: scores[result] + 6,
-  };
-}
-
-export function getInferiorHand(winningHand: string) {
-  const result = ranks[getHand(winningHand)][0];
-
-  if (!result) {
-    throw Error(`couldnt find losing hand to ${winningHand}`);
-  }
-
-  // console.log(`inferior: ${winningHand} beats: ${result}`);
-
-  return {
-    outcome: "lose",
-    result,
-    score: scores[result] + 0,
-  };
-}
-
-export function getDrawHand(drawHand: string) {
-  const result = getHand(drawHand);
-
-  if (!result) {
-    throw Error(`couldnt find losing hand to ${drawHand}`);
-  }
-
-  // console.log(`draw: ${result} is a draw hand to ${drawHand}`);
-
-  return {
-    outcome: "draw",
-    result,
-    score: scores[result] + 3,
-  };
-}
-
-function getResponse(turn: string, response: string) {
-  switch (response) {
-    // lose
-    case "X":
-      return getInferiorHand(turn);
-    // draw
-    case "Y":
-      return getDrawHand(turn);
-    // win
-    case "Z":
-      return getSuperiorHand(turn);
-    default:
-      throw new Error(
-        `cannot get response to ${turn} with response ${response}`
-      );
-  }
-}
-
-export async function main2(file: InputFileType) {
-  const input = await getInput(2, file);
-  const totalScore = input.split("\n").reduce((acc, line) => {
-    if (!/[ABCXYZ]/.test(line)) {
+  const sum = data.split("\n").reduce((acc, line) => {
+    if (/done/i.test(line)) {
       return acc;
     }
 
-    const [turn, response] = line.split(" ");
-
-    const nextResponse = getResponse(turn, response);
-
-    return acc + nextResponse.score;
+    const [p1, p2] = splitCompartments(line);
+    const commonItem = findCommonItems(p1, p2);
+    const itemPriority = getPriority(commonItem);
+    return acc + itemPriority;
   }, 0);
 
-  return totalScore;
+  return sum;
+}
+
+export function findCommonGroupItems(backpackGroups: string[]) {
+  const allChars = new Set(backpackGroups.join(""));
+  const characterSets = backpackGroups.map(
+    (backpackChars) => new Set(backpackChars)
+  );
+
+  let commonChar;
+  for (const [item] of allChars.entries()) {
+    let count = 0;
+    for (const charSet of characterSets) {
+      if (charSet.has(item)) {
+        count += 1;
+      } else {
+        continue;
+      }
+    }
+    if (count == 3) {
+      commonChar = item;
+      break;
+    }
+  }
+
+  return commonChar;
+}
+
+export function chunk(input: any[], count: number) {
+  return input.reduce<string[][]>((acc, current) => {
+    let currentBatch = acc[acc.length - 1] || [];
+    if (currentBatch.length < count) {
+      currentBatch.push(current);
+      acc[acc.length - 1 < 0 ? 0 : acc.length - 1] = currentBatch;
+    } else {
+      acc.push([current]);
+    }
+    return acc;
+  }, []);
+}
+
+export async function main2(file: InputFileType) {
+  const data = await getInput(3, file);
+  const lines = data.split("\n");
+  const chunkedLines = chunk(lines, 3);
+
+  const sum = chunkedLines.reduce((total, groupArray) => {
+    const commonItem = findCommonGroupItems(groupArray);
+
+    if (!commonItem) return total;
+
+    return total + getPriority(commonItem);
+  }, 0);
+
+  return sum;
 }
